3769b06da8a100743dc2cf7567deec563a9c9d4a
Sammai Gutierrez <sammaigtz@hotmail.com>
2021-05-04 14:18:54 -0700

    Add functionality to main() for user interaction.
    Co-Authored-by: Michael Burton <micbur1@gmail.com>

--- EpicProjects/IntroWeek-C#/TriangleTracker.Solution/TriangleTracker/Models/TriangleTracker.cs
+++ EpicProjects/IntroWeek-C#/TriangleTracker.Solution/TriangleTracker/Models/TriangleTracker.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace TriangleDetector
 {
   public class Triangle
@@ -6,13 +8,19 @@ namespace TriangleDetector
 
     public string CompareSides(int sideOne, int sideTwo, int sideThree)
     {
-      if ((sideOne + sideTwo) > sideThree)
+      if ((sideOne + sideTwo) <= sideThree || (sideTwo + sideThree) <= sideOne || (sideOne + sideThree) <= sideTwo)
       {
         return "This is not a triangle.";
       }
+      else if (sideOne == sideTwo && sideOne == sideThree)
+      {
+        return "This is an equalateral triangle.";
+      }else if(sideOne == sideTwo || sideOne == sideThree || sideTwo == sideThree){
+        return "This is an Isosceles triangle.";
+      }
       else
       {
-        return "aaaaaaaaaaa";
+        return "This is a Scalene triangle.";
       }
     }
   }
--- EpicProjects/IntroWeek-C#/TriangleTracker.Solution/TriangleTracker/Program.cs
+++ EpicProjects/IntroWeek-C#/TriangleTracker.Solution/TriangleTracker/Program.cs
@@ -1,13 +1,26 @@
 using System;
 using System.Collections.Generic;
 
+
 namespace TriangleDetector
 {
   public class Program
   {
     public static void Main()
     {
-      Console.WriteLine("Hello");
+      Triangle myTriangle = new Triangle();
+      Console.WriteLine("Welcome to triangle the identifyer");
+      Console.WriteLine("Plz enter a number for the fisrt side of your triangle.");
+      string stringSideOne = Console.ReadLine();
+      Console.WriteLine("Plz enter a number for the second side of your triangle.");
+      string stringSideTwo = Console.ReadLine();
+      Console.WriteLine("Plz enter a number for the third side of your triangle.");
+      string stringSideThree = Console.ReadLine();
+      int intSideOne = int.Parse(stringSideOne);
+      int intSideTwo = int.Parse(stringSideTwo);
+      int intSideThree = int.Parse(stringSideThree);
+      string result = myTriangle.CompareSides(intSideOne,intSideTwo, intSideThree);
+      Console.WriteLine(result);
     }
   }
 }
