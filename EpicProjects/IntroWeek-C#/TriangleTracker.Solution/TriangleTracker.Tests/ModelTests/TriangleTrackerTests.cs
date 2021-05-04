e68bc246ec26cb0adbb8b9ad725bdb9617736e78
Sammai Gutierrez <sammaigtz@hotmail.com>
2021-05-04 13:38:05 -0700

    Fail 3rd test. > > Co-Authored-by: Michael Burton <micbur1@gmail.com>

--- EpicProjects/IntroWeek-C#/TriangleTracker.Solution/TriangleTracker.Tests/ModelTests/TriangleTrackerTests.cs
+++ EpicProjects/IntroWeek-C#/TriangleTracker.Solution/TriangleTracker.Tests/ModelTests/TriangleTrackerTests.cs
@@ -20,6 +20,13 @@ namespace TriangleDetector.Tests
       string result = "This is an equalateral triangle.";
       Assert.AreEqual(result, testCompareSides.CompareSides(2,2,2));
     }
+    [TestMethod]
+    public void CompareSides_IfSidesMakeTriangle_thisIsAnIsoscelesTriangle()
+    {
+      Triangle testCompareSides = new Triangle();
+      string result = "This is an Isosceles triangle.";
+      Assert.AreEqual(result, testCompareSides.CompareSides(2,2,3));
+    }
     // Test methods go here
   }
 }
