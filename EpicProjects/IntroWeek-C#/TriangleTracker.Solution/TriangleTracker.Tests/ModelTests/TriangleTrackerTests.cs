13041fb85e8b5b41a10ecb72002a608301b4df54
Sammai Gutierrez <sammaigtz@hotmail.com>
2021-05-04 13:45:39 -0700

    Fail 4th test.
    Co-Authored-by: Michael Burton <micbur1@gmail.com>

--- EpicProjects/IntroWeek-C#/TriangleTracker.Solution/TriangleTracker.Tests/ModelTests/TriangleTrackerTests.cs
+++ EpicProjects/IntroWeek-C#/TriangleTracker.Solution/TriangleTracker.Tests/ModelTests/TriangleTrackerTests.cs
@@ -21,11 +21,18 @@ namespace TriangleDetector.Tests
       Assert.AreEqual(result, testCompareSides.CompareSides(2,2,2));
     }
     [TestMethod]
-    public void CompareSides_IfSidesMakeTriangle_thisIsAnIsoscelesTriangle()
+    public void CompareSides_IfSidesMakeIsoscelsTriangle_thisIsAnIsoscelesTriangle()
     {
       Triangle testCompareSides = new Triangle();
       string result = "This is an Isosceles triangle.";
       Assert.AreEqual(result, testCompareSides.CompareSides(2,2,3));
+    }
+    [TestMethod]
+    public void CompareSides_IfSidesMakeScaleneTriangle_thisIsAScaleneTriangle()
+    {
+      Triangle testCompareSides = new Triangle();
+      string result = "This is a Scalene triangle.";
+      Assert.AreEqual(result, testCompareSides.CompareSides(4,2,3));
     }
     // Test methods go here
   }
